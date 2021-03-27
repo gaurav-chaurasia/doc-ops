fd40dad82f62f46a48f3b455bc3ef65d9fa3d54f
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-03-27 22:37:22 +0530

    initialize project

---
+++ app.js
@@ -0,0 +1,25 @@
+// node modules
+const express = require('express');
+const app     = express();
+const multer  = require('multer');
+const uuid    = require('uuid').v4;
+const path    = require('path');
+const layout  = require("express-ejs-layouts");
+
+// local node modules
+const ROUTER  = require('./routes');
+
+
+app.use(layout);
+app.set("layout", "./v1/layouts/layout");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// setting router
+app.use(ROUTER);
+
+
+module.exports = app;
---
+++ controllers/home.controller.js
@@ -0,0 +1,3 @@
+exports.getHome = (req, res) => {
+    res.render("v1/index");
+};
