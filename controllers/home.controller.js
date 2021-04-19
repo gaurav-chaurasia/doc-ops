9306729710f8a2fc557ffafdf87615cf36f45f04
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-04-19 20:47:18 +0530

    views for upload doc ready

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,6 +2,5 @@ const Logger = require('../config/logger').init('MASTER');
 
 exports.getHome = (req, res) => {
     Logger.info(`check`);
-    res.status(200);
-    res.render('v1/index');
+    res.status(200).render('v1/index');
 };
